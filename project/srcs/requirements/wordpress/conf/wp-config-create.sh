e77c5327da81d0e71e949c5207a009bbd8c98f4b
marie <marie@msebbane>
2023-07-05 17:06:55 +0200

    wordpress fini

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,23 +1,12 @@
-#!bin/sh
-if [ ! -f "/var/www/wp-config.php" ]; then
-cat << EOF > /var/www/wp-config.php
-<?php
-define( 'DB_NAME', '${DB_NAME}' );
-define( 'DB_USER', '${DB_USER}' );
-define( 'DB_PASSWORD', '${DB_PASS}' );
-define( 'DB_HOST', 'mariadb' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
-define('FS_METHOD','direct');
-\$table_prefix = 'wp_';
-define( 'WP_DEBUG', false );
-if ( ! defined( 'ABSPATH' ) ) {
-define( 'ABSPATH', __DIR__ . '/' );}
-define( 'WP_REDIS_HOST', 'redis' );
-define( 'WP_REDIS_PORT', 6379 );
-define( 'WP_REDIS_TIMEOUT', 1 );
-define( 'WP_REDIS_READ_TIMEOUT', 1 );
-define( 'WP_REDIS_DATABASE', 0 );
-require_once ABSPATH . 'wp-settings.php';
-EOF
-fi
+#!/bin/sh
+
+cd /var/www/
+
+echo "hello"
+wp core install --path=/var/www/ --url=msebbane.42.fr --title=inception --admin_user=msbbane --admin_password=inception --admin_email=mariesebbane@gmail.com --skip-email --allow-root
+
+echo "check"
+wp theme install teluro --path=/var/www/ --activate --allow-roo wp user create leon leon@le.on --role=author --path=/var/www/ --user_pass=marie --allow-root
+
+exec /usr/sbin/php-fpm81 -F
+
