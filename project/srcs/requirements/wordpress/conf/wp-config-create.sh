a85ebd1cc3fddcb0227cb936f6f71ffb73196bfc
marie <marie@msebbane>
2023-07-06 15:14:10 +0200

    modif

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+#wp core download --locale=fr_FR --allow-root
 cd /var/www/
 
-echo "hello"
-wp core install --path=/var/www/ --url=msebbane.42.fr --title=inception --admin_user=msbbane --admin_password=inception --admin_email=mariesebbane@gmail.com --skip-email --allow-root
+echo "Install admin user"
+wp core install --path=/var/www/ --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$DB_USER --admin_password=$DB_PASS --admin_email=$WP_EMAIL --skip-email --allow-root
 
-echo "check"
-wp theme install teluro --path=/var/www/ --activate --allow-roo wp user create leon leon@le.on --role=author --path=/var/www/ --user_pass=marie --allow-root
+echo "Install theme"
+wp theme install twentytwentytwo --path=/var/www/ --activate --allow-root
+
+echo "Create user"
+wp user create $WP_NEW_USER newuser@inception.fr --role=author --path=/var/www/ --user_pass=$WP_NEW_PASS --allow-root
 
 exec /usr/sbin/php-fpm81 -F
 
